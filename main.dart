f17d1a4d2a1595ea7755e0caf295ec3c86a7c4ed
Edilson Ceará <93453573+edilsonceara01@users.noreply.github.com>
2021-11-24 02:38:04 -0300

    Código tela inicial

--- main.dart
+++ main.dart
@@ -2,165 +2,91 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Home(),                           //Classe criada em outro arquivo (Jogo.dart)
+    home: Home(),
     debugShowCheckedModeBanner: false,
   ));
 }
 
-
+//O metodo build armazena a execução da classe Home
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.cinzinha,
-      appBar: AppBar(
-        title: Text("Meu Perfil"),
-        backgroundColor: Colors.azulMoney,
-        leading: IconButton(
-          onPressed: () {},
-          icon: Icon(Icons.arrow_back_rounded),
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Center(
-        child: Container(
-        child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,       //Alinhamento da coluna no centro
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Padding(
-              padding: EdgeInsets.only(top: 32,bottom: 0),
-              child: Icon(
-                Icons.account_circle_outlined,
-                color: Colors.azulMoney,
-                size: 120,
-              )
-          ),
-
-          Padding(
-              padding: EdgeInsets.only(top: 0,bottom: 16),
-              child: GestureDetector(
-                onTap: (){print("Tocou viu");},
-                child: Text(
-                  "Alterar foto do perfil",
+      backgroundColor: Color(0xFF2D3E9C),
+      //Definindo a cor do plano de fundo em hexadecimal
+      body: Container(
+        padding: EdgeInsets.all(16),
+        width: double.infinity, //Container se adapta a largura de cada dispositivo
+        child: Column(          //Definindo coluna
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Column(children: [
+              Image.asset("Images/mymoney.png", //Logo do app
+                  scale: 0.75, //Ajustando o tamanho da imagem
+                  width: 700,
+                  height: 350),
+              SizedBox(height: 50), //Colocando espaçamento entre widgets
+              Text(             //Texto abaixo da logo
+                  "Gestão simplificada de suas finanças!",
+                  textAlign: TextAlign.justify,
                   style: TextStyle(
-                      color: Colors.azulMoney,
-                      fontSize: 15,
-                      decoration: TextDecoration.underline
+                      fontFamily: "RedRose",
+                      fontSize: 20,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.white)),
+              SizedBox(height: 150), //Colocando espaçamento entre widgets
+              ButtonTheme(
+                //Botão Começar
+                minWidth: 400.0, //Definindo dimensões do botão
+                height: 50.0,
+                child: RaisedButton(
+                  shape: RoundedRectangleBorder(
+                      //Configurando borda
+                      borderRadius: BorderRadius.circular(18.0),
+                      side: BorderSide(color: Colors.cinzinha)),
+                  padding: EdgeInsets.fromLTRB(30, 10, 30, 10),
+                  onPressed: () {},
+                  child: Text(
+                    "Começar",
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
                   ),
+                  color: Colors.pinkMoney,
                 ),
-              )
-          ),
-          Row(
-            children: [
-              Padding(
-                  padding: EdgeInsets.only(left:10,top: 50,bottom: 0),
+              ),
+              SizedBox(height: 10),
+              ButtonTheme(
+                //Botão Já sou Cadastrado
+                minWidth: 400.0,
+                height: 50.0,
+                child: RaisedButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                      side: BorderSide(color: Colors.pinkMoney)),
+                  padding: EdgeInsets.fromLTRB(30, 10, 30, 10),
+                  onPressed: () {},
                   child: Text(
-                    "Nome:",
+                    "Já sou cadastrado",
                     style: TextStyle(
-                      color: Colors.azulMoney,
-                      fontSize: 15,
-                    ),
-                  )
-              )
-            ],
-          ),
-          Padding(
-              padding: EdgeInsets.only(left:10,top: 0,bottom: 0, right: 10),
-                  child: TextField(
-
-          )
-          ),
-          Row(
-            children: [
-              Padding(
-                  padding: EdgeInsets.only(left:10,top: 20,bottom: 0),
-                  child: Text(
-                    "Email:",
-                    style: TextStyle(
-                      color: Colors.azulMoney,
-                      fontSize: 15,
-                    ),
-                  )
-              )
-            ],
-          ),
-          Padding(
-              padding: EdgeInsets.only(left:10,top: 0,bottom: 0, right: 10),
-              child: TextField(
-
-              )
-          ),
-          Row(
-            children: [
-              Padding(
-                  padding: EdgeInsets.only(left:10,top: 20,bottom: 0),
-                  child: Text(
-                    "Nova senha:",
-                    style: TextStyle(
-                      color: Colors.azulMoney,
-                      fontSize: 15,
-                    ),
-                  )
-              )
-            ],
-          ),
-          Padding(
-              padding: EdgeInsets.only(left:10,top: 0,bottom: 0, right: 10),
-              child: TextField(
-
-              )
-          ),
-          Row(
-            children: [
-              Padding(
-                  padding: EdgeInsets.only(left:10,top: 20,bottom: 0),
-                  child: Text(
-                    "Confirmar nova senha:",
-                    style: TextStyle(
-                      color: Colors.azulMoney,
-                      fontSize: 15,
-                    ),
-                  )
-              )
-            ],
-          ),
-          Padding(
-              padding: EdgeInsets.only(left:10,top: 0,bottom: 0, right: 10),
-              child: TextField(
-
-              )
-          ),
-        SizedBox(height: 40),
-        ButtonTheme(
-          //Botão Atualizar
-          minWidth: 300.0,
-          height: 50.0,
-          child: RaisedButton(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(18.0),
+                        fontSize: 20,
+                        color: Colors.pinkMoney,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  color: Colors.cinzinha,
                 ),
-            padding: EdgeInsets.fromLTRB(30, 10, 30, 10),
-            onPressed: () {},
-            child: Text(
-              "Atualizar",
-              style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold),
-            ),
-            color: Colors.azulMoney,
-          )
+              ),
+            ])
+          ],
         ),
-        ],
       ),
-    ))));
+    );
   }
 }
-
