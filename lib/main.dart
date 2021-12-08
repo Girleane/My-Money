52991a14a42cbb1ccb90b879761efcc5c2a26fd0
Girleane <girleanee@gmail.com>
2021-12-08 17:49:45 -0300

    testando firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth_platform_interface/firebase_auth_platform_interface.dart';
 import 'package:flutter/material.dart';
 import 'package:my_money_integrated/InicialPage.dart';
 
-void main() {
+void main() async {
   runApp(MaterialApp(
     home: InicialPage(),
     theme: ThemeData(
         hintColor: Colors.white,
         primaryColor: Colors.white,
-        shadowColor: Colors.black,),
+        shadowColor: Colors.black,)
   ));
+}
+
+
+void register (BuildContext context) async{
+  FirebaseUSer
+
 }
